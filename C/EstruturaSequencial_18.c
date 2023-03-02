dc41f9c7616c5524bdad7f431ffaa4d2c54f7f15
Lucas Ribeiro <108464429+arueiralucas@users.noreply.github.com>
2023-03-02 19:36:08 -0300

    Update EstruturaSequencial_18.c

--- C/EstruturaSequencial_18.c
+++ C/EstruturaSequencial_18.c
@@ -3,6 +3,11 @@
 //velocidade de um link de Internet (em Mbps), calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
 
 int main () {
-
-
+ float kb, mb;
+  printf("Digite o tamanho do arquivo em MB : \n");
+  scanf( "%f", &kb);
+  //1min = 60 seg 60Mbps
+  mb=kb/60;
+  printf("O tempo de download deste aquivo é de %.2f minutos.",mb);
+  return (0);
 }
