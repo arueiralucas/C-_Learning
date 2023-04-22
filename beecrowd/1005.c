aaba27781187b07ea95a707fd479eab24456642a
Lucas Ribeiro <108464429+arueiralucas@users.noreply.github.com>
2023-04-22 14:47:51 -0300

    beecrowd c

---
+++ beecrowd/1005.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+float a;
+float b;
+float media;
+scanf("%f%f",&a,&b);
+media = ((a*3.5) + (b*7.5))/11;
+
+printf("MEDIA = %.5f\n",media);
+
+
+    return 0;
+}
