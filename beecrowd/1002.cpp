277f3ad2eb16fea19dd699193a54abd08579dbf4
Lucas Ribeiro <108464429+arueiralucas@users.noreply.github.com>
2023-07-21 02:33:22 -0300

    beecrowd c

--- beecrowd/1002.cpp
+++ beecrowd/1002.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 int main (){
-float n =3.14159;
-float raio;
+double n =3.14159;
+double raio;
 cin >> raio;
+cout << fixed << setprecision(4);
 cout << "A=" << n*(raio*raio)<<endl;
 return 0; 
 
